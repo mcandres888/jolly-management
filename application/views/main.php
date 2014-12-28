b9551ae6758bb8850b39040d914a95132ccf14f9
Marc Clemen Andres <mcandres@Marcs-MacBook-Pro.local>
2014-12-28 14:02:37 +0800

    added  bootstrap

---
+++ application/views/main.php
@@ -0,0 +1,297 @@
+    
+<?php include 'header.php'?>
+<!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <div class="fill" style="background-image:url('<?=base_url('images')?>/slider-bc.png');"></div>
+                <div class="carousel-caption">
+                    <h2>Providing Solutions for your manpower needs</h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('<?=base_url('images')?>/aw-shared-slider-005.jpg');"></div>
+                <div class="carousel-caption">
+                    <h2>Serving you with a smile</h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('<?=base_url('images')?>/slider_buildings.jpg');"></div>
+                <div class="carousel-caption">
+                    <h2>Providing Solutions for your manpower needs</h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Jolly Management Solutions Inc.
+                </h1>
+            </div>
+
+            <div class="col-lg-12">
+
+           <p>
+The company was incorporated on April 29, 2005 to engage in the business of local contracting, enlisting, recruiting professionals, skilled, unskilled and individuals or firms in the supply of manpower.  
+</p><p>
+
+JMSI, has been supplying manpower nationwide with experience in Luzon, Visayas and Mindanao evidenced by the clientele in our map where we are deploying manpower. We ensure quality and competitiveness of our workers. We invest efforts in mobilizing highly qualified workers to suit the requirements and to the satisfaction of our clients. 
+</p>
+<p>
+JMSI is committed to creating a highly interactive relationship to provide our clients with the best possible service tailored and to suit their specific needs while aiming to provide quality service at a very reasonable and competitive cost.   
+</p>
+<p>
+
+JMSI also have not lost sight that its objective is to assist Filipinos in securing promising job opportunities nationwide in order to help them augment their needs and attain economic stability for their families.
+
+JMSI believes that to obtain the success of a business of this kind aside from the combined effort between the company and its clienteles the company should also have an understanding of its client’s business.  Thus, our company place a huge emphasis on taking a full client briefing and thereby get a full understanding of their needs.  The objective is to transpose that need into a viable recruitment action plan.
+           </p>
+
+<br><br>
+	   </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-check"></i> Our Vision and Mission</h4>
+                    </div>
+                    <div class="panel-body">
+                        <h4>Our Mission</h4>
+			<p>
+                      Explore the market needs and match it with JMSI strength and core competence in manpower supply to it’s valued clients. 
+			</p>
+                        <h4>Our Vision</h4>
+                        <p>
+                        For the company to master the core competence of effective skills matching the least possible time to provide value and enhance our Clients operations and business.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-gift"></i> Our Core Asset</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p><b>JMSI</b> prides itself in having people with wide range of experience in respective areas of expertise, who are always looking for improvements to meet the needs of our clients.  This is why we see the people in our organization as our core asset.</p>
+<p>  <b>JMSI</b> has built a good working relationship amongst its staff premised on trust, confidence and teamwork which results in harmony and team efforts that promotes effectiveness and result.
+</p> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4><i class="fa fa-fw fa-compass"></i> Our Core Value</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p><b>JMSI</b> guiding principles hinge on our ideologies with emphasis on:</p>
+<p> Providing clients with superior services. Intimate knowledge of our clients and their business activities. </p>
+<p> Providing workplace that is pleasant and comfortable to our employees. Commitment in pursuit of higher goals.
+</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Portfolio Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Clients</h2>
+            </div>
+            <div class="col-md-4 col-sm-6">
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_yacht_club.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_ace.png">
+             </div>
+
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_brady.gif">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sti.JPG">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sti_santos.jpeg">
+             </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+             <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sti_medical.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sti_medical_clinic.jpg">
+             </div>
+
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_pacific.jpeg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sm_masinag.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_md.jpg">
+             </div>
+
+            </div>
+            <div class="col-md-4 col-sm-6">
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sm_pasig.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_vista.jpg">
+             </div>
+
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sm_taytay.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_best_mobile.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_canadian.png">
+             </div>
+
+
+            </div>
+            <div class="col-md-4 col-sm-6">
+             <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sm_pasig.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_vista.jpg">
+             </div>
+
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sm_taytay.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_best_mobile.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_canadian.png">
+             </div>
+
+
+            </div>
+            <div class="col-md-4 col-sm-6">
+             <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_yacht_club.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_ace.png">
+             </div>
+
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_brady.gif">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sti.JPG">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sti_santos.jpeg">
+             </div>
+           
+            </div>
+            <div class="col-md-4 col-sm-6">
+             <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sti_medical.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sti_medical_clinic.jpg">
+             </div>
+
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_pacific.jpeg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_sm_masinag.jpg">
+             </div>
+              <div id="logo_div">
+                 <img src="<?=base_url('images')?>/logo_md.jpg">
+             </div>
+
+
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Features Section -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Service Offered</h2>
+            </div>
+            <div class="col-md-6">
+                <p>Jolly Management Solutions Inc. offers the following services:</p>
+                <ul>
+                    <li>Assist Client with manpower need from end to end hiring process
+</li>
+                    <li>Support employee by providing security Assist Client with manpower need from end to end hiring process
+</li>
+                </ul>
+                <br>
+               <h3>Our Specialization</h3>
+                <p>Jolly Management Solution Inc. specializes the following: </p>
+                 <ul>
+                    <li>Housekeeping/Janitorial Service </li>
+                    <li>Service Crew for Food and Beverages </li> 
+      <li>Receptionist/ Room Attendant  for Hotel and Hospitality </li>
+                    <li>Drivers</li>
+                    <li>Cashier</li>
+                    <li>Auto Mechanic</li>
+                    <li>IT Support</li>
+                    <li>Administrative Work</li>
+                    <li>Customer Service Relation</li>
+                    <li>Production Worker</li>
+                    <li>Telemarketer</li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="<?=base_url('images')?>/services.jpg" alt="">
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Call to Action Section -->
+        <div class="well">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>For inquiries send us an email hrdrecruitment.jmsi@gamil.com or hrdrecruitment.jmsi@yahoo.com. You can also inquire by using our inquiry form.</p>
+                </div>
+                <div class="col-md-4">
+                    <a class="btn btn-lg btn-default btn-block" href="#">Send an inquiry</a>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+
+<?php include 'footer.php'?>
